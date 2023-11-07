8fd0b9113f24377faf3179b8df2c5eacef6ee4f6
Esha  Vijay <eshavijay@Eshas-MacBook-Air.local>
2023-11-07 17:54:06 -0500

    Test case for Filter by Amount

--- expense_tracker/test/TestExample.java
+++ expense_tracker/test/TestExample.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.text.ParseException;
@@ -14,6 +15,7 @@ import org.junit.Test;
 import controller.ExpenseTrackerController;
 import model.ExpenseTrackerModel;
 import model.Transaction;
+import model.Filter.AmountFilter;
 import view.ExpenseTrackerView;
 
 
@@ -115,55 +117,91 @@ public class TestExample {
     }
 
     @Test
-  public void testAddFoodTransaction() {
+    public void testAddFoodTransaction() {
     // Pre-condition: Ensure the transaction list is initially empty
-    assertEquals("The transaction list should be empty before adding a new transaction.", 0, model.getTransactions().size());
+        assertEquals("The transaction list should be empty before adding a new transaction.", 0, model.getTransactions().size());
 
     // Action: Add a transaction with amount 50.00 and category "food"
-    double amount = 50.0;
-    String category = "food";
-    boolean addTransactionResult = controller.addTransaction(amount, category);
+        double amount = 50.0;
+        String category = "food";
+        boolean addTransactionResult = controller.addTransaction(amount, category);
 
     // Verify: Check if the transaction was added successfully
-    assertTrue("The transaction should be added successfully.", addTransactionResult);
+        assertTrue("The transaction should be added successfully.", addTransactionResult);
 
     // Post-condition: The transaction list should contain the new transaction
-    List<Transaction> transactions = model.getTransactions();
-    assertEquals("The transaction list should contain one transaction after adding.", 1, transactions.size());
+        List<Transaction> transactions = model.getTransactions();
+        assertEquals("The transaction list should contain one transaction after adding.", 1, transactions.size());
 
     // Verify: The transaction details are correct
-    Transaction transaction = transactions.get(0);
-    checkTransaction(amount, category, transaction);
+        Transaction transaction = transactions.get(0);
+        checkTransaction(amount, category, transaction);
 
     // Verify: The total cost is updated correctly
-    double totalCost = getTotalCost();
-    assertEquals("The total cost should be updated to reflect the added transaction amount.", amount, totalCost, 0.01);
-  }
+        double totalCost = getTotalCost();
+        assertEquals("The total cost should be updated to reflect the added transaction amount.", amount, totalCost, 0.01);
+    }
 
-  @Test
-  public void testInvalidInputHandling() {
+    @Test
+    public void testInvalidInputHandling() {
     // Pre-condition: Ensure the transaction list is initially empty
-    assertEquals("The transaction list should be empty before attempting to add an invalid transaction.", 0, model.getTransactions().size());
+        assertEquals("The transaction list should be empty before attempting to add an invalid transaction.", 0, model.getTransactions().size());
 
     // Save the initial total cost
-    double initialTotalCost = getTotalCost();
+        double initialTotalCost = getTotalCost();
 
     // Action: Attempt to add a transaction with an invalid amount (e.g., -50.0) and invalid category (e.g., "")
-    double invalidAmount = -50.0; // Negative amount to represent an invalid case
-    String invalidCategory = ""; // Empty string to represent an invalid case
-    boolean addTransactionResult = controller.addTransaction(invalidAmount, invalidCategory);
+        double invalidAmount = -50.0; // Negative amount to represent an invalid case
+        String invalidCategory = ""; // Empty string to represent an invalid case
+        boolean addTransactionResult = controller.addTransaction(invalidAmount, invalidCategory);
 
     // Verify: Adding transaction should fail
-    assertFalse("Adding an invalid transaction should fail.", addTransactionResult);
+        assertFalse("Adding an invalid transaction should fail.", addTransactionResult);
 
     // Post-condition: Verify the transaction list is still empty
-    List<Transaction> transactions = model.getTransactions();
-    assertEquals("The transaction list should remain empty after attempting to add an invalid transaction.", 0, transactions.size());
+        List<Transaction> transactions = model.getTransactions();
+        assertEquals("The transaction list should remain empty after attempting to add an invalid transaction.", 0, transactions.size());
 
     // Verify: Total cost should remain unchanged
-    double totalCostAfterAttempt = getTotalCost();
-    assertEquals("The total cost should remain unchanged after attempting to add an invalid transaction.", initialTotalCost, totalCostAfterAttempt, 0.01);
-}
+        double totalCostAfterAttempt = getTotalCost();
+        assertEquals("The total cost should remain unchanged after attempting to add an invalid transaction.", initialTotalCost, totalCostAfterAttempt, 0.01);
+    }
+    @Test
+    public void testFilterByAmount() {
+    // Pre-condition: Ensure the transaction list is initially empty
+        assertEquals("The transaction list should be empty before adding transactions.", 0, model.getTransactions().size());
+
+    // Action: Add multiple transactions with different amounts
+        controller.addTransaction(50.0, "food");
+        controller.addTransaction(75.0, "utilities");
+        controller.addTransaction(50.0, "entertainment");
+
+    // Set the filter to the controller with the specified amount
+        double filterAmount = 50.0;
+        controller.setFilter(new AmountFilter(filterAmount));
+
+    // Apply the filter
+        controller.applyFilter();
+
+    // Retrieve the filtered transactions after filter application
+        List<Transaction> allTransactions = model.getTransactions();
+        List<Transaction> filteredTransactions = new ArrayList<>();
+        for (Transaction t : allTransactions) {
+            if (t.getAmount() == filterAmount) {
+                filteredTransactions.add(t);
+            }
+        }
+
+    // Verify: Check if the filtered transactions list contains only the transactions with the specified amount
+        assertEquals("The filtered transactions list should contain only transactions matching the filter amount.", 2, filteredTransactions.size());
+        for (Transaction transaction : filteredTransactions) {
+            assertEquals("Filtered transaction should match the filter amount.", filterAmount, transaction.getAmount(), 0.01);
+        }
+    }
+
+
+    
+
 
     
 }
